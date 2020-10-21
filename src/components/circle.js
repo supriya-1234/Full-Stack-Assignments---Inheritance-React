cb4f1b2036dd21c12147fd545067f43e0d1ef5ce
supriya-1234 <66170913+supriya-1234@users.noreply.github.com>
2020-10-21 19:30:49 +0530

    changes

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,7 +1,12 @@
-// import 
-
+// import
+import Shape from "./shape";
+class Circle extends Shape {
+  constructor(color) {
+    super(color);
+  }
+  calculateArea() {}
+}
 // declare class
-
+export default Circle;
 
 // export
-
